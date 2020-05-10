eb2553877a817553cc94b6c248b8d65692a45201
JoshuaRotimi <62189959+JoshuaRotimi@users.noreply.github.com>
2020-05-10 21:55:52 +0100

    Updates

---
+++ precache-manifest.f4c229d311bacbd46fd7a1cf0af3e4a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0240dcaf8b299e9d14342f404cda3ac",
+    "url": "/contactlist/index.html"
+  },
+  {
+    "revision": "ead57562d9444d6b5099",
+    "url": "/contactlist/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fee3a7d8e6cbe390ee95",
+    "url": "/contactlist/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ead57562d9444d6b5099",
+    "url": "/contactlist/static/js/2.df031d99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactlist/static/js/2.df031d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee3a7d8e6cbe390ee95",
+    "url": "/contactlist/static/js/main.97ecf687.chunk.js"
+  },
+  {
+    "revision": "0107d86ed9f7b6deaf5d",
+    "url": "/contactlist/static/js/runtime-main.e8b5211d.js"
+  }
+]);
